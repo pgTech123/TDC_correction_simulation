475945fdbe28e97976fb633e07bf645fdb84026a
pgTech123 <pgendron40@gmail.com>
2019-12-09 17:21:33 -0500

    Added ICSSHSR5 transfer function algorithm for comparison

--- main.py
+++ main.py
@@ -2,20 +2,30 @@ import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 from transfer_function_ideal import TransferFunctionIdeal
+from transfer_function_ICSSHSR4 import TransferFunctionICSSHSR4
 
 
 def main():
+    fig, ax = plt.subplots()
+
     filename = "./../data/Demo_Uncorrelated_coefficients_4.txt"
     tf_ideal = TransferFunctionIdeal(filename=filename)
+    # Note: The linear regression algorithm seems to give better results
+    tf_icsshsr4 = TransferFunctionICSSHSR4(tf_ideal, algorithm="linear_regression")
 
+    tf_graph_ideal = tf_ideal.get_transfer_functions_raw_data()
+    tf_graph_icsshsr4 = tf_icsshsr4.get_transfer_functions_raw_data()
 
-    tf_graph = tf_ideal.get_transfer_functions_raw_data()
+    #ax.plot(tf_graph_ideal[0][1], tf_graph_ideal[1][1])
+    #ax.plot(tf_graph_icsshsr4[0][1], tf_graph_icsshsr4[1][1])
+
+    ax.plot(tf_graph_ideal[0][1], tf_graph_ideal[1][1]-tf_graph_icsshsr4[1][1])
+
     inl = tf_ideal.get_inl_data()
     dnl = tf_ideal.get_dnl_data()
-    fig, ax = plt.subplots()
     #ax.plot(tf_graph[0][1], tf_graph[1][1])
     #ax.plot(inl[0][1], inl[1][1])
-    ax.plot(dnl[0][1], dnl[1][1])
+    #ax.plot(dnl[0][1], dnl[1][1])
 
     plt.show()
 
--- transfer_function_ICSSHSR4.py
+++ transfer_function_ICSSHSR4.py
@@ -1,9 +1,127 @@
+import copy
+import numpy as np
+
+########################################################
+# CONSTANTS
+########################################################
+NUMBER_OF_TDC = 256
+########################################################
 
 
 class TransferFunctionICSSHSR4:
     def __init__(self, transfer_function_ideal, algorithm="linear_regression"):
-        self.active_tdc = transfer_function_ideal.get_active_tdc()
-        self.max_fine = transfer_function_ideal.get_max_fine()
-        ps_per_coarse = transfer_function_ideal.get_ps_per_coarse()
-        [x, raw_data] = transfer_function_ideal.get_transfer_functions_raw_data()
+        self.transfer_function_ideal = transfer_function_ideal
+        if algorithm == "linear_regression":
+            self._linear_regression_algorithm()
+        elif algorithm == "median":
+            self._median_algorithm()
+        else:
+            raise Exception("Invalid algorithm")
 
+    # Returns an array of size 256 containing true of false
+    def get_mask_active_tdc(self):
+        return self.transfer_function_ideal.get_mask_active_tdc()
+
+    # Returns an array containing the ID of the active TDCs
+    def get_active_tdc(self):
+        return self.transfer_function_ideal.get_active_tdc()
+
+    # Returns a copy of the transfer function raw data
+    def get_transfer_functions_raw_data(self):
+        # Make a copy of the list so that someone
+        # outside this file cannot modify it.
+        return [copy.deepcopy(self.x), copy.deepcopy(self.y_tf)]
+
+    # TODO: Returns a copy of the inl raw data
+    def get_inl_data(self):
+        raise Exception("Function not implemented yet")
+        # Make a copy of the list so that someone
+        # outside this file cannot modify it.
+        return [copy.deepcopy(self.x), copy.deepcopy(self.inl)]
+
+
+    # TODO: Returns a copy of the dnl raw data
+    def get_dnl_data(self):
+        raise Exception("Function not implemented yet")
+        # Make a copy of the list so that someone
+        # outside this file cannot modify it.
+        return [copy.deepcopy(self.x), copy.deepcopy(self.dnl)]
+
+    def evaluate(self, coarse, fine, tdc):
+        return coarse * self.coarse_period[tdc] + fine * self.fine_period[tdc]
+
+    #######################################################################
+    #                      PRIVATE FUNCTIONS
+    #       (These functions shouldn't be called from outside)
+    #######################################################################
+    @staticmethod
+    def _get_median_step(y_tf):
+        y = np.array(y_tf)
+        diff = np.diff(y)
+        median_step = np.median(diff)
+        return median_step
+
+    @staticmethod
+    def _linear_regression(y_tf):
+        number_of_points = len(y_tf)
+        if number_of_points < 5:
+            return None
+        Y = np.transpose(np.array([y_tf]))
+        x = np.arange(number_of_points)
+        A_t = np.vstack((x, np.ones(number_of_points)))
+        A = np.transpose(A_t)
+        mult_A_t_Y = np.dot(A_t, Y)
+        mult_A_t_A = np.dot(A_t, A)
+        mult_A_t_A_inv = np.linalg.inv(mult_A_t_A)
+        parameters = np.dot(mult_A_t_A_inv, mult_A_t_Y)
+        return parameters[:, 0]
+
+    def _linear_regression_algorithm(self):
+        self.active_tdc = self.transfer_function_ideal.get_active_tdc()
+        max_fine = self.transfer_function_ideal.get_max_fine()
+        [x, raw_data] = self.transfer_function_ideal.get_transfer_functions_raw_data()
+
+        self.coarse_period = np.zeros(NUMBER_OF_TDC)
+        self.fine_period = np.zeros(NUMBER_OF_TDC)
+        for tdc in range(NUMBER_OF_TDC):
+            if tdc not in self.active_tdc:
+                continue
+            parameters = self._linear_regression(raw_data[tdc])
+            if parameters is None:
+                continue
+            a = parameters[0]
+            b = parameters[1]
+            median_step = self._get_median_step(raw_data[tdc])
+            self.fine_period[tdc] = np.around(median_step)  # The height of the median step seems to be a good approximation.
+            self.coarse_period[tdc] = np.around(a*max_fine[tdc])
+        self._compute_transfer_function_y(x)
+
+    def _median_algorithm(self):
+        self.active_tdc = self.transfer_function_ideal.get_active_tdc()
+        ps_per_coarse = self.transfer_function_ideal.get_ps_per_coarse()
+        [x, raw_data] = self.transfer_function_ideal.get_transfer_functions_raw_data()
+
+        self.coarse_period = np.zeros(NUMBER_OF_TDC)
+        self.fine_period = np.zeros(NUMBER_OF_TDC)
+        for i in range(NUMBER_OF_TDC):
+            if i not in self.active_tdc:
+                continue
+            median_step = self._get_median_step(raw_data[i])
+            self.fine_period[i] = np.around(median_step)  # The height of the median step seems to be a good approximation.
+            self.coarse_period[i] = np.around(ps_per_coarse[i])
+        self._compute_transfer_function_y(x)
+
+    def _compute_transfer_function_y(self, x):
+        y_tf = []
+        max_fine = self.transfer_function_ideal.get_max_fine()
+        for tdc in range(NUMBER_OF_TDC):
+            y_estimated = []
+            for x_value in x[tdc]:
+                coarse = int(x_value / max_fine[tdc])
+                fine = int(x_value % max_fine[tdc])
+                estimated_time = self.evaluate(coarse, fine, tdc)
+                y_estimated.append(estimated_time)
+            y_estimated_arr = np.array(y_estimated)
+            y_tf.append(y_estimated_arr)
+        self.x = x
+        self.y_tf = y_tf
